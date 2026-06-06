c7e9c1ef92a9a64eaa1e91a41b161616818d3981
ratanratan <ratna5256@gmail.com>
2026-06-06 12:06:16 +0530

    commit by ratan cupon data

--- src/CartSlice.js
+++ src/CartSlice.js
@@ -1,37 +1,37 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 let cartSlice = createSlice({
-    name: "cart",
-    initialState: [],
-    reducers: {
-        addToCart: (state, action) => {
-            let existingItem = state.find(item => item.name === action.payload.name)
-            if (existingItem) {
-                existingItem.quantity += 1;
-            }
-            else {
-                let finalObject = { ...action.payload, quantity: 1 };
-                state.push(finalObject);
-            }
-        },
-        removeCart: (state, action) => {
+  name: "cart",
+  initialState: [],
+  reducers: {
+    addToCart: (state, action) => {
+      let existingItem = state.find(
+        (item) => item.name === action.payload.name,
+      );
+      if (existingItem) {
+        existingItem.quantity += 1;
+      } else {
+        let finalObject = { ...action.payload, quantity: 1 };
+        state.push(finalObject);
+      }
+    },
+    removeCart: (state, action) => {
+      let index = state.findIndex((item) => item.name === action.payload.name);
 
-            let index = state.findIndex(item => item.name === action.payload.name);
+      if (index != -1) {
+        state.splice(index, 1);
+      }
+    },
+    clearCart: () => {
+      return [];
+    },
 
-            if (index != -1) {
-                state.splice(index, 1);
-            }
-
-        },
-        clearCart: () => { return [] }
-    }
+    // This is Ratan sir
+  },
 });
 
-
-
-
-//export the reducer then imported by component  
+//export the reducer then imported by component
 export const { addToCart, removeCart, clearCart } = cartSlice.actions;
 
-//Export the slice then imported by store 
-export default cartSlice.reducer
+//Export the slice then imported by store
+export default cartSlice.reducer;
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,16 +1,18 @@
 import { useNavigate } from "react-router-dom";
 
 function Home() {
+  let navigate = useNavigate();
 
-    let navigate = useNavigate();
-
-    return (
-        <>
-            <h1>Home Page</h1>
-            <p>Welcome to the homepage. Click the button below to view your profile.</p>
-            <button onClick={() => navigate("/profile")}>Profile</button>
-        </>
-    )
+  return (
+    <>
+      <h1>Home Page</h1>
+      <p>
+        Welcome to the homepage. Click the button below to view your profile.
+      </p>
+      <button onClick={() => navigate("/profile")}>Profile</button>
+      //Today class completedd......
+    </>
+  );
 }
 
 export default Home;
